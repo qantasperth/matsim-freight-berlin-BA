4378247cbdb98398eba55e2b7d6d46dbfec36f9b
timur <57106306+qantasperth@users.noreply.github.com>
2023-02-24 21:23:35 +0100

    added sample carrier.xml and vehicleTypes.xml added RunMatsimFreightBerlin class

---
+++ src/main/java/org/matsim/project/RunMatsimFreightBerlin.java
@@ -0,0 +1,64 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.project;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.freight.FreightConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.scenario.ScenarioUtils;
+
+/**
+ * @author nagel
+ *
+ */
+public class RunMatsimFreightBerlin {
+
+	public static void main(String[] args) {
+
+		Config config;
+		if ( args==null || args.length==0 || args[0]==null ){
+			config = ConfigUtils.loadConfig( "scenarios/equil/config.xml" );
+			config.plans().setInputFile(null);
+			config.controler().setOutputDirectory(".output/freight");
+			config.controler().setLastIteration(0);
+
+			FreightConfigGroup freightConfigGroup = ConfigUtils.addOrGetModule(config, FreightConfigGroup.class);
+			freightConfigGroup.setCarriersFile(".scenarios/equil/carrier.xml");
+			freightConfigGroup.setCarriersVehicleTypesFile(".scenarios/equil/vehicleTypes.xml");
+		} else {
+			config = ConfigUtils.loadConfig( args );
+		}
+
+		// setting network input file
+		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
+		config.network().setInputFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-network.xml.gz");
+
+		
+		Scenario scenario = ScenarioUtils.loadScenario(config) ;
+		
+		
+		Controler controler = new Controler( scenario ) ;
+		controler.run();
+	}
+	
+}
+
