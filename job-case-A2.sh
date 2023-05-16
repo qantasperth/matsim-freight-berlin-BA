41bb0b4126d8cb309f2484b24d7fd55a218e08e5
timur <57106306+qantasperth@users.noreply.github.com>
2023-05-16 21:53:59 +0200

    added .sh job files

---
+++ job-case-A2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash --login
+#$ -l h_rt=48000
+#$ -N test
+#$ -o ./logfile_$JOB_NAME.log
+#$ -j y
+#$ -m abe
+#$ -M bogautdinov@campus.tu-berlin.de
+#$ -cwd
+#$ -pe mp 12
+#$ -l mem_free=12G
+date
+hostname
+runId=$JOB_NAME
+#used memory Java
+java_memory="-Xmx130G"
+classpath="matsim-freight-BA-b4a8641.jar"
+echo ""
+echo "classpath: $classpath"
+echo ""
+java command
+java_command="java -Djava.awt.headless=true $java_memory -cp $classpath"
+main
+main="org/matsim/project/run/RunMatsimFreightBerlinBA"
+arguments
+arguments="A2"
+command
+command="$java_command $main $arguments"
+echo ""
+echo "command is $command"
+echo ""
+echo "using alternative java"
+module add java/17
+java -version
+$command
